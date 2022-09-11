6093a564879a0f787ac383fa5b471598a7dcb91e
Sarina Hemmat pour <hemmatpour.sarina@gmail.com>
2022-09-11 16:13:10 +0430

    delete, copy and move features for both files and folders

--- app/src/main/java/com/example/filemanagement/FileListFragment.java
+++ app/src/main/java/com/example/filemanagement/FileListFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,13 +16,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Objects;
 
-public class FileListFragment extends Fragment {
+public class FileListFragment extends Fragment implements FilesAdapter.FileAdapterCallBack {
 
     private String path;
+
+    private static File source;
+    private static File destination;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -59,16 +64,7 @@ public class FileListFragment extends Fragment {
         rvFiles=view.findViewById(R.id.rv_fragmentFileList_files);
         rvFiles.setLayoutManager(new LinearLayoutManager(getContext() , LinearLayoutManager.VERTICAL , false));
 
-        adapter=new FilesAdapter(Arrays.asList(Objects.requireNonNull(currentFile.listFiles())),
-                new FilesAdapter.FileAdapterCallBack() {
-                    @Override
-                    public void onFileClicked(File file) {
-                        if (file.isDirectory())
-                        {
-                            ( (MainActivity) requireActivity()).showFilesFragment(file.getPath() , true);
-                        }
-                    }
-                });
+        adapter=new FilesAdapter(Arrays.asList(Objects.requireNonNull(currentFile.listFiles())), this);
 
         rvFiles.setAdapter(adapter);
 
@@ -98,4 +94,70 @@ public class FileListFragment extends Fragment {
         }
     }
 
+    @Override
+    public void onFileClicked(File file) {
+        if (file.isDirectory())
+        {
+            ( (MainActivity) requireActivity()).showFilesFragment(file.getPath() , true);
+        }
+    }
+
+    @Override
+    public void onDeleteItemClicked(File file) {
+        if (FileOperationUtil.delete(file))
+        {
+            String name=file.getName();
+            adapter.deleteFile(file);
+            Toast.makeText(getContext(), name+" file/folder is deleted", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    public void onMoveItemClicked(File file) {
+        File des=getDesFolder();
+        if (!des.exists()){
+            if (des.mkdir())
+            {
+                adapter.addFile(des);
+                rvFiles.scrollToPosition(0);
+            }
+        }
+        try {
+            if (FileOperationUtil.move(file , des))
+            {
+                adapter.deleteFile(file);
+                Toast.makeText(getContext(), file.getName() + " file/folder is moved", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getContext(), ":|", Toast.LENGTH_SHORT).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCopyItemClicked(File file) {
+        File des=getDesFolder();
+        if (!des.exists()){
+            if (des.mkdir())
+            {
+                adapter.addFile(des);
+                rvFiles.scrollToPosition(0);
+            }
+        }
+        try {
+            FileOperationUtil.copy(file , des);
+            Toast.makeText(getContext(), file.getName() + " file/folder is copied", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public File getDesFolder()
+    {
+        return new File(requireActivity().getExternalFilesDir(null).getPath()+File.separator+"Destination");
+    }
 }
--- app/src/main/java/com/example/filemanagement/FilesAdapter.java
+++ app/src/main/java/com/example/filemanagement/FilesAdapter.java
@@ -1,12 +1,15 @@
 package com.example.filemanagement;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.File;
@@ -43,10 +46,12 @@ public class FilesAdapter extends RecyclerView.Adapter<FilesAdapter.FileViewHold
 
         TextView tvName;
         ImageView imgType;
+        View btnMore;
         public FileViewHolder(@NonNull View itemView) {
             super(itemView);
             tvName=itemView.findViewById(R.id.tv_item_file);
             imgType=itemView.findViewById(R.id.img_itemFile_type);
+            btnMore=itemView.findViewById(R.id.img_itemFile_more);
         }
 
         public void bind(File file){
@@ -64,6 +69,39 @@ public class FilesAdapter extends RecyclerView.Adapter<FilesAdapter.FileViewHold
                     callback.onFileClicked(file);
                 }
             });
+
+            btnMore.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    //creating a popup menu
+                    PopupMenu popupMenu=new PopupMenu(view.getContext() , view); //mige be kodum view bechasbe
+                    popupMenu.getMenuInflater().inflate(R.menu.menu_file_operations , popupMenu.getMenu());
+                    popupMenu.show();
+
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @SuppressLint("NonConstantResourceId")
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId())
+                            {
+                                case R.id.menu_delete:
+                                    callback.onDeleteItemClicked(file);
+                                    break;
+                                case R.id.menu_copy:
+                                    callback.onCopyItemClicked(file);
+                                    break;
+                                case R.id.menu_move:
+                                    callback.onMoveItemClicked(file);
+                                    break;
+                            }
+
+                            return false;
+                        }
+                    });
+
+                }
+            });
         }
 
 
@@ -71,11 +109,24 @@ public class FilesAdapter extends RecyclerView.Adapter<FilesAdapter.FileViewHold
 
     public interface FileAdapterCallBack {
         void onFileClicked(File file);
+        void onDeleteItemClicked(File file);
+        void onMoveItemClicked(File file);
+        void onCopyItemClicked(File file);
     }
 
     public void addFile(File newFolder)
     {
         files.add(0 , newFolder);
         notifyItemInserted(0);
+    }
+
+    public void deleteFile(File file)
+    {
+        int index=files.indexOf(file);
+        if (index>-1)
+        {
+            files.remove(index);
+            notifyItemRemoved(index);
+        }
     }
 }
